d81c34d5852799aa8de80810bba052782cbf055d
yellow sky <hellun515@gmail.com>
2025-03-12 17:11:18 +0900

    init c# project

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace InputMacro
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.dgvDataView = new System.Windows.Forms.DataGridView();
+      this.btnExecute = new System.Windows.Forms.Button();
+      this.lbStatus = new System.Windows.Forms.Label();
+      this.txbDataPath = new System.Windows.Forms.ComboBox();
+      this.nudSpeed = new System.Windows.Forms.NumericUpDown();
+      this.label1 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.dgvDataView)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.nudSpeed)).BeginInit();
+      this.SuspendLayout();
+
+      // 
+      // dgvDataView
+      // 
+      this.dgvDataView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+      this.dgvDataView.Location = new System.Drawing.Point(12, 39);
+      this.dgvDataView.Name = "dgvDataView";
+      this.dgvDataView.Size = new System.Drawing.Size(470, 291);
+      this.dgvDataView.TabIndex = 0;
+
+      // 
+      // btnExecute
+      // 
+      this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.btnExecute.Location = new System.Drawing.Point(12, 336);
+      this.btnExecute.Name = "btnExecute";
+      this.btnExecute.Size = new System.Drawing.Size(75, 23);
+      this.btnExecute.TabIndex = 2;
+      this.btnExecute.Text = "실행 (F9)";
+      this.btnExecute.UseVisualStyleBackColor = true;
+      this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+
+      // 
+      // lbStatus
+      // 
+      this.lbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+      this.lbStatus.Location = new System.Drawing.Point(93, 336);
+      this.lbStatus.Name = "lbStatus";
+      this.lbStatus.Size = new System.Drawing.Size(389, 23);
+      this.lbStatus.TabIndex = 3;
+      this.lbStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+
+      // 
+      // txbDataPath
+      // 
+      this.txbDataPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+      this.txbDataPath.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.txbDataPath.FormattingEnabled = true;
+      this.txbDataPath.Location = new System.Drawing.Point(12, 12);
+      this.txbDataPath.Name = "txbDataPath";
+      this.txbDataPath.Size = new System.Drawing.Size(351, 20);
+      this.txbDataPath.TabIndex = 4;
+      this.txbDataPath.SelectedIndexChanged += new System.EventHandler(this.txbDataPath_SelectedIndexChanged_1);
+
+      // 
+      // nudSpeed
+      // 
+      this.nudSpeed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.nudSpeed.Location = new System.Drawing.Point(418, 11);
+      this.nudSpeed.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+      this.nudSpeed.Name = "nudSpeed";
+      this.nudSpeed.Size = new System.Drawing.Size(64, 21);
+      this.nudSpeed.TabIndex = 5;
+
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.label1.Location = new System.Drawing.Point(369, 11);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(43, 21);
+      this.label1.TabIndex = 6;
+      this.label1.Text = "속도";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+      // 
+      // Form1
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+      this.ClientSize = new System.Drawing.Size(494, 371);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.nudSpeed);
+      this.Controls.Add(this.txbDataPath);
+      this.Controls.Add(this.lbStatus);
+      this.Controls.Add(this.btnExecute);
+      this.Controls.Add(this.dgvDataView);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Input Macro 1.2";
+      this.TopMost = true;
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.dgvDataView)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.nudSpeed)).EndInit();
+      this.ResumeLayout(false);
+    }
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.NumericUpDown nudSpeed;
+    private System.Windows.Forms.ComboBox txbDataPath;
+    private System.Windows.Forms.Label lbStatus;
+    private System.Windows.Forms.Button btnExecute;
+    private System.Windows.Forms.DataGridView dgvDataView;
+
+    #endregion
+  }
+}
---
+++ Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace InputMacro
+{
+  public partial class Form1 : Form
+  {
+
+    Excel.Application excelApplication = null;
+    Excel.Workbook workbook = null;
+    Excel.Worksheet worksheet = null;
+
+    private bool _started;
+    private bool started {
+      get => _started;
+      set {
+        _started = value;
+        Invoke((MethodInvoker)delegate { btnExecute.Text = _started ? "중지 (F9)" : "실행 (F9)"; });
+      }
+    }
+
+    private bool _standby = false;
+    private bool standby {
+      get => _standby;
+      set {
+        _standby = value;
+        Invoke((MethodInvoker)delegate { btnExecute.Enabled = _standby; });
+      }
+    }
+    private CancellationTokenSource cts = new CancellationTokenSource();
+    private GlobalHook kHook;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      standby = false;
+      var di = new DirectoryInfo(Environment.CurrentDirectory);
+      txbDataPath.Items.AddRange(di.GetFiles("*.xls").Concat(di.GetFiles("*.xlsx")).Select(x => x.FullName).ToArray<object>());
+
+      kHook = new GlobalHook();
+      kHook.AddKeyDownHandler(KeyDownHandler);
+      kHook.Begin();
+
+      if (txbDataPath.Items.Count > 0)
+        txbDataPath.Text = txbDataPath.Items[0].ToString();
+    }
+    private void KeyDownHandler(object sender, KeyEventArgs e)
+    {
+      if (e.KeyCode == Keys.F9)
+      {
+        Toggle();
+      }
+    }
+
+    private void LoadData(string path)
+    {
+      var t = new Task(() => {
+        Invoke((MethodInvoker)delegate {
+          _standby = false;
+          nudSpeed.Value = 0;
+          SetStatus("Preparing Data Load...", Color.Orange);
+          SetStatus("Clearing Sheets...", Color.Orange);
+          dgvDataView.Columns.Clear();
+          try
+          {
+            SetStatus("Open Excel Files...", Color.Orange);
+            excelApplication = new Excel.Application
+            {
+              Visible = false
+            };
+            workbook = excelApplication.Workbooks.Open(path);
+            worksheet = (Excel.Worksheet)workbook.Worksheets[1];
+
+            // var range = worksheet.Range["C3 : F102"];
+            //
+            // for (int row = 1; row <= range.Rows.Count; row++)
+            // {
+            //   var rv = range.Cells[row, 1] as Excel.Range;
+            //   if (rv == null || rv.Value2 == null) continue;
+            //   dgvDataView.Rows.Add();
+            //   for (int column = 1; column <= range.Columns.Count; column++)
+            //   {
+            //     var value = range.Cells[row, column] as Excel.Range;
+            //     dgvDataView[column - 1, row - 1].Value = value.Value2;
+            //     SetStatus($"Load Cell: ({row}, {column}) {value.Value2}", Color.Orange);
+            //   }
+            // }
+            SetStatus($"Load Interval...", Color.Orange);
+            var interval = worksheet.Cells[2, 2] as Excel.Range;
+            if (interval?.Value != null && int.TryParse(interval.Value.ToString(), out var itv))
+              nudSpeed.Value = itv ;
+            
+            var r = 0;
+            SetStatus($"Load Columns...", Color.Orange);
+            while (true)
+            {
+              var columnHeader = worksheet.Cells[2, 3 + r] as Excel.Range;
+              if (columnHeader?.Value == null) break;
+              dgvDataView.Columns.Add($"a{r}", $"{columnHeader.Value2}");
+              SetStatus($"Load Column: {columnHeader.Value}", Color.Orange);
+              r++;
+            }
+
+            SetStatus($"Load Rows...", Color.Orange);
+            var c = 0;
+            while (true)
+            {
+              var any = false;
+              var ls = new List<object>();
+              for (int i = 0; i < r; i++)
+              {
+                var x = worksheet.Cells[3 + c, 3 + i] as Excel.Range;
+                ls.Add(x?.Value);
+                if (x?.Value != null && x.Value.ToString() != "")
+                  any = true;
+
+                // break;
+              }
+              if (!any) break;
+              dgvDataView.Rows.Add();
+
+              for (var i = 0; i < ls.Count; i++)
+              {
+                if (ls[i] == null) continue;
+                dgvDataView[i, c].Value = ls[i];
+                SetStatus($"Load Cell[{i}, {c}]: {ls[i]}", Color.Orange);
+              }
+
+              c++;
+            }
+          }
+          catch (Exception exc)
+          {
+            MessageBox.Show(exc.Message);
+          }
+          finally
+          {
+            workbook.Close(false);
+            excelApplication.Quit();
+            ReleaseObject(workbook);
+            ReleaseObject(worksheet);
+            ReleaseObject(excelApplication);
+            standby = true;
+            SetStatus("StandBy", Color.Green);
+          }
+        });
+      });
+      t.Start();
+    }
+
+    private void SetStatus(string text) => SetStatus(text, Color.Black);
+    private void SetStatus(string text, Color color)
+    {
+      Invoke((MethodInvoker)delegate {
+        lbStatus.ForeColor = color;
+        lbStatus.Text = text;
+      });
+    }
+
+    static void ReleaseObject(object obj)
+    {
+      try
+      {
+        if (obj != null)
+        {
+          Marshal.ReleaseComObject(obj); // 액셀 객체 해제
+          obj = null;
+        }
+      }
+      catch (Exception ex)
+      {
+        obj = null;
+        throw ex;
+      }
+      finally
+      {
+        GC.Collect(); // 가비지 수집
+      }
+    }
+
+    private void Execute()
+    {
+      started = true;
+      SetStatus("Starting Macro", Color.Blue);
+      Task.Factory.StartNew(Exe, cts.Token);
+    }
+
+    private async void Exe()
+    {
+      try
+      {
+        for (var row = 0; row < dgvDataView.Rows.Count; row++)
+        {
+          for (var column = 0; column < dgvDataView.Columns.Count; column++)
+          {
+            var value = dgvDataView[column, row].Value;
+            if (value == null || string.IsNullOrEmpty(value.ToString())) continue;
+            SetStatus($"Send: {value}", Color.Blue);
+            dgvDataView.ClearSelection();
+            dgvDataView[column, row].Selected = true;
+            SendKeys.SendWait(value.ToString());
+            if(nudSpeed.Value > 0)
+              await Task.Delay((int)nudSpeed.Value);
+            cts.Token.ThrowIfCancellationRequested();
+          }
+        }
+        started = false;
+        SetStatus("Standby", Color.Green);
+      }
+      catch(OperationCanceledException exc)
+      {
+        started = false;
+        SetStatus("StandBy", Color.Green);
+      }
+    }
+
+    private void Toggle()
+    {
+      if (!standby) return;
+      if (started)
+      {
+        SetStatus("Request Cancel...", Color.Green);
+        cts.Cancel();
+      }
+      else
+      {
+        cts = new CancellationTokenSource();
+        Execute();
+      }
+    }
+
+    private void txbDataPath_SelectedIndexChanged_1(object sender, EventArgs e)
+    {
+      LoadData(txbDataPath.Text);
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      try
+      {
+        workbook.Close(false);
+        excelApplication.Quit();
+        ReleaseObject(workbook);
+        ReleaseObject(worksheet);
+        ReleaseObject(excelApplication);
+      }
+      catch
+      {
+      }
+    }
+    private void btnExecute_Click(object sender, EventArgs e)
+    {
+      Toggle();
+    }
+  }
+}
